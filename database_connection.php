7a80c36873cd35bd6b54d29a28996de73acde18d
Ronan Spaan <44583172+SlendyBlue@users.noreply.github.com>
2019-11-08 16:45:53 +0100

    database connection, functions, index, en test bestand toegevoegd

--- database_connection.php
+++ database_connection.php
@@ -1,7 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Ronan
- * Date: 8-11-2019
- * Time: 16:36
- */
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'wideworldimporters';
+$db_port = 3306;
+
+$db_conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
+if ($db_conn->connect_error) {
+    $error = $db_conn->connection_error;
+}
+echo"great work!";
